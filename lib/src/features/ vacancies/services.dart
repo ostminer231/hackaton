8b9d3308fd39a7ddd585b5551ffaa9fd5b3fa291
Capralll <vladislav170486@gmail.com>
2023-11-26 01:10:40 +0500

    сервис

--- lib/src/features/ vacancies/services.dart
+++ lib/src/features/ vacancies/services.dart
@@ -2,13 +2,164 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'package:hackaton/src/constants/colors.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Remove debug banner
+      home: ServicesPage(),
+    );
+  }
+}
+
 class ServicesPage extends StatelessWidget {
   const ServicesPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-
+    return Scaffold(
+      body: Column(
+        children: [
+          YourCustomHeader(),
+          SizedBox(height: 16), // Add some space between the headers
+          ServicesMontage(),
+        ],
+      ),
     );
   }
+}
+
+class YourCustomHeader extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 108,
+      width: double.infinity,
+      margin: EdgeInsets.only(bottom: 8),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 3),
+          ),
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      // Adjusted content position
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(height: 66), // Add some space above the text
+          Text(
+            'Услуги',
+            style: TextStyle(
+              fontSize: 20,
+              color: primaryColor,
+            ),
+          ),
+        ],
+      ),
+    );
   }
+}
+
+class ServicesMontage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 362,
+      margin: EdgeInsets.only(bottom: 8),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 3),
+          ),
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Text(
+              'Монтажник',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Icon(
+              IconData(0xf63b, fontFamily: 'MaterialIcons', matchTextDirection: true),
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class Services2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 362,
+      margin: EdgeInsets.only(bottom: 8),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withOpacity(0.3),
+            spreadRadius: 2,
+            blurRadius: 4,
+            offset: Offset(0, 3),
+          ),
+        ],
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 16),
+            child: Text(
+              'Монтажник',
+              style: TextStyle(
+                fontSize: 18,
+                color: Colors.black,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(right: 16),
+            child: Icon(
+              IconData(0xf63b, fontFamily: 'MaterialIcons', matchTextDirection: true),
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
