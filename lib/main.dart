a587d45f6e92b62149a95aa3c6d5e89db94ee80f
miner212321321 <117258266+ostminer231@users.noreply.github.com>
2023-11-25 15:02:31 +0500

    asdasd

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'src/features/auth/splash_screen/splash_screen.dart';
 import 'src/features/auth/onboarding_screens/onboarding.dart';
 import 'src/features/auth/register_screen/register.dart';
+import 'src/features/auth/register_screen/role.dart';
 
 
 void main() => runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       home: const SplashScreen(),
       routes: {
         '/session_1/onboarding': (context) => const OnBoarding(),
-        '/session_2/register': (context) => const Register(),
+        '/session_2/register': (context) => const RoleScreen(),
       },
     );
   }
